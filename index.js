3a53fd7847dcaf9c5e1f96a1b4af9fea788fdc8f
David Castaneda <davidcastaneda@Davids-MBP.hsd1.fl.comcast.net>
2020-05-03 14:17:35 -0400

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const { PORT = 8080, TEST_ENV = "TEST_ENV not set" } = process.ENV;
+
+app.get("/", (req, res) => res.status(200).send("docker-to-node"));
+
+app.get("/env", (req, res) => res.status(200).json({ env: TEST_ENV }));
+
+app.get("/ping", (req, res) => res.status(200).send("pong"));
+
+app.get("/health", (req, res) =>
+  res.status(200).json({ status: 200, message: "healthy" })
+);
+
+app.listen(PORT, () => console.log(`> Listening :${PORT}`));
